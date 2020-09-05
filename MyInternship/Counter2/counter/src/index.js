3fff0a51b5b8364e2710a54bc9155bb7965e8101
Yun Miao <one.missing.variable@gmail.com>
2020-09-04 19:08:25 -0700

    counter using function

--- MyInternship/Counter2/counter/src/index.js
+++ MyInternship/Counter2/counter/src/index.js
@@ -1,46 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Counter extends React.Component {
-  constructor(props) {    
-    super(props);    
-    this.state = {      
-      count: 0,    
-    };  
-  }
-
-  increment(num) {
-    this.setState(state => ({
-      count : Math.max(0, state.count + num)
-    }));
-  }
-
-  render() {
+function Counter() {
+  const [count, setCount] = useState(0);
     return (
       <div class="content">
         <h1 class="center"><b>Counter</b></h1>
-        <h2 class="center" id="count">{this.state.count}</h2>
+        <h2 class="center" id="count">{count}</h2>
         <div class="center buttons-wrapper"> 
           <button 
           class="plus button black" 
-          onClick={() => this.increment(1)}>
+          onClick={() => setCount(count + 1)}>
             +1
           </button> 
           <button 
           class="minus button black" 
-          onClick={() => this.increment(-1)}>
+          onClick={() => setCount(Math.max(0, count-1))}>
             -1
           </button>
           <button 
           class="reset button black" 
-          onClick={() => this.setState({count: 0})}>
+          onClick={() => setCount(0)}>
             reset
           </button>
         </div>
       </div>
     );
-  }
 }
 
 
